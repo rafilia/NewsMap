f81fd942fbc0ed12012b8360246b897805caa690
rafilia <con.mono.cro@gmail.com>
2015-07-20 01:49:30 +0900

    add jsoup and checked able to get main text

--- app/src/main/java/jp/ac/titech/itpro/sdl/newsmap/RSSLoader.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/newsmap/RSSLoader.java
@@ -9,6 +9,8 @@ import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 
+import org.jsoup.Jsoup;
+
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -26,6 +28,7 @@ public class RSSLoader extends AsyncTask<String, Integer, ArrayList<NewsInfo>> {
         mNewsInfo = newsinfo;
     }
 
+    // show Progress Bar during execution
     @Override
     protected void onPreExecute() {
         mProgressDialog = new ProgressDialog(mMapsActivity);
@@ -36,14 +39,34 @@ public class RSSLoader extends AsyncTask<String, Integer, ArrayList<NewsInfo>> {
     @Override
     protected ArrayList<NewsInfo> doInBackground(String... params) {
         try{
+            // Get RSS Feed
             URL feedurl = new URL(params[0]);
             SyndFeedInput input = new SyndFeedInput();
             SyndFeed feed = input.build(new XmlReader(feedurl));
 
+            // Search Location
             for(Object obj: feed.getEntries()){
+                // Get RSS entry info
                 SyndEntry entry = (SyndEntry) obj;
-                Log.i(TAG, entry.getTitle());
-                Log.i(TAG, entry.getLink());
+                String title = entry.getTitle();
+                String entry_url = entry.getLink();
+
+                Log.i(TAG, title);
+                Log.i(TAG, entry_url);
+
+                // GET 'Detailed Link'
+                org.jsoup.nodes.Document doc = Jsoup.connect(entry_url).get();
+                String detailed_link = doc.select("a.newsLink").first().attr("href");
+
+                Log.i(TAG, detailed_link);
+
+                // GET main article
+                org.jsoup.nodes.Document doc2 = Jsoup.connect(detailed_link).get();
+                String main_text = doc2.select("p.ynDetailText").first().text();
+
+                Log.i(TAG, main_text);
+
+                // find location info
             }
 
         } catch (Exception e){
