108cf80cfbe7ec1329ca5cf0a6dc815d44dce04d
rafilia <con.mono.cro@gmail.com>
2015-07-19 23:17:00 +0900

    add NewsInfo class

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/newsmap/NewsInfo.java
@@ -0,0 +1,18 @@
+package jp.ac.titech.itpro.sdl.newsmap;
+
+/**
+ * Created by tm on 2015/07/19.
+ */
+public class NewsInfo {
+    private String title;
+    private String URL;
+    private boolean has_location;
+    private String Location;
+
+    public NewsInfo() {
+        title="";
+        URL="";
+        has_location=false;
+        Location="";
+    }
+}
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/newsmap/RSSParser.java
@@ -0,0 +1,15 @@
+package jp.ac.titech.itpro.sdl.newsmap;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by tm on 2015/07/19.
+ */
+public class RSSParser extends AsyncTask<String, Integer, NewsInfo> {
+
+
+    @Override
+    protected NewsInfo doInBackground(String... strings) {
+        return null;
+    }
+}
